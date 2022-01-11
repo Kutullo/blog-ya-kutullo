b163beeb27ef333a2cb631d8404afff72e94b0bd
Kutullo <47230220+Kutullo@users.noreply.github.com>
2022-01-11 11:39:28 +0200

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = ['blog-ya-kutullo.herokuapp.com','127.0.0.1:8000']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
+    'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -139,7 +141,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT=os.path.join(BASE_DIR,'staticflies')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 STATIC_FILES_DIR =[os.path.join(BASE_DIR,'static'),]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
